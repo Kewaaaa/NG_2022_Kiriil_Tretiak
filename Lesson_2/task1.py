aa6ce37986236fb62bfd422639ea7047b7b49464
Kewaaaa <inokenty119228@gmail.com>
2022-11-15 20:25:34 +0200

    Lists and cycles

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,4 +1,10 @@
 value = list(input('Enter a variable: '))
 print("Lenght: " + str(len(value)))
-
-
+uniq = set(value)
+for element in value:
+    print(element, end="")
+print("\n")
+l = list(uniq)
+l.sort()
+for element in l:
+    print(element + " - " + str(value.count(element)))
---
+++ Lesson_2/task5.py
@@ -0,0 +1,4 @@
+s = input("Enter a variable: ").split(", ")
+print("Min: " + min(s))
+print("Max: " + max(s))
+
