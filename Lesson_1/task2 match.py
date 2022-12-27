85da2ca805b7bfcacab4781aca8e46d7a096a0ad
Kewaaaa <inokenty119228@gmail.com>
2022-12-27 14:33:14 +0200

    calculator

--- Lesson_1/task2 match.py
+++ Lesson_1/task2 match.py
@@ -10,15 +10,7 @@ match symbolForCalc:
         print("Result: " + str(firstNumberForCalc * secondNumberForCalc))
     case "^":
         print("Result: " + str(firstNumberForCalc ** secondNumberForCalc))
-match secondNumberForCalc:
-    case 0:
-        print("you can't")
     case "/":
         print("Result: " + str(firstNumberForCalc / secondNumberForCalc))
-if firstNumberForCalc < 0:
-    firstNumberForCalc = 0
-match firstNumberForCalc:
-    case 0:
-        print("you can't")
     case "&":
         print("Result " + str(firstNumberForCalc ** (1 / secondNumberForCalc)))
