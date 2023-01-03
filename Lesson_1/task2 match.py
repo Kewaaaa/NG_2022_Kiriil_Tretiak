8d8571ec0e21791c85de42c66d3f1f61bf6d9d27
Kewaaaa <inokenty119228@gmail.com>
2023-01-03 14:03:24 +0200

    Added files

--- Lesson_1/task2 match.py
+++ Lesson_1/task2 match.py
@@ -1,17 +1,17 @@
 firstNumberForCalc = int(input("Enter first number: "))
 secondNumberForCalc = int(input("Enter second number: "))
 symbolForCalc = input("Which operation u want: ")
-result = "Result: "
 match symbolForCalc:
     case "+":
-        print(result + str(firstNumberForCalc + secondNumberForCalc))
+        result = firstNumberForCalc + secondNumberForCalc
     case "-":
-        print(result + str(firstNumberForCalc - secondNumberForCalc))
+        result = firstNumberForCalc - secondNumberForCalc
     case "*":
-        print(result + str(firstNumberForCalc * secondNumberForCalc))
+        result = firstNumberForCalc * secondNumberForCalc
     case "^":
-        print(result + str(firstNumberForCalc ** secondNumberForCalc))
+        result = firstNumberForCalc ^ secondNumberForCalc
     case "/":
-        print(result + str(firstNumberForCalc / secondNumberForCalc))
+        result = firstNumberForCalc / secondNumberForCalc
     case "&":
-        print(result + str(firstNumberForCalc ** (1 / secondNumberForCalc)))
+        result = firstNumberForCalc ** (1/secondNumberForCalc)
+print("Result: " + str(result))
